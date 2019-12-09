3a69bf3d81f438944c3b3e354eb6bdf6e2645db7
Breno Hennrique <hennrique.breno@gmail.com>
2019-12-09 16:29:06 -0300

    Adiciona profile

--- src/helpers/redmine.js
+++ src/helpers/redmine.js
@@ -13,9 +13,59 @@ class RedmineHelper {
     this.redmine = new Redmine(hostname, config)
   }
 
-  getIssues (options) {
+  getIssues (options = { limit: 2 }) {
     return new Promise((resolve, reject) => {
       this.redmine.issues(options, (error, data) => {
+        if (error) {
+          reject(error)
+        } else {
+          resolve(data)
+        }
+      })
+    })
+  }
+
+  getIssueById (id, params = {}) {
+    return new Promise((resolve, reject) => {
+      this.redmine.get_issue_by_id(id, params, (error, data) => {
+        if (error) {
+          reject(error)
+        } else {
+          resolve(data)
+        }
+      })
+    })
+  }
+
+  createIssue (subject, description) {
+    return new Promise((resolve, reject) => {
+      const options = {
+        issue: {
+          project_id: 1,
+          tracker_id: 2,
+          author_id: 2,
+          subject,
+          description
+        }
+      }
+      custom_fields
+      console.log(options)
+
+      this.redmine.create_issue(options, (error, data) => {
+        if (error) {
+          reject(error)
+        } else {
+          resolve(data)
+        }
+      })
+    })
+  }
+
+  // Funcoes a analizar
+
+  getCustomFields () {
+    return new Promise((resolve, reject) => {
+      this.redmine.custom_fields((error, data) => {
         if (error) {
           reject(error)
         } else {
--- src/intents/fallback/defaultIntent.js
+++ src/intents/fallback/defaultIntent.js
@@ -5,10 +5,10 @@ import RedmineHelper from '../../helpers/redmine'
 
 const defaultIntent = (_agent, _params) => {
   return new Promise((resolve, reject) => {
-    RedmineHelper.getIssues({ limit: 2 }).then(response => {
+    debug('Default Intent!')
+    RedmineHelper.getIssueById(3).then(response => {
       console.log('===>')
       console.log(response)
-      debug('Default Intent!')
       resolve()
     }, error => {
       console.log(error)
