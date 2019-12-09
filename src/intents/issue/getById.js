61b139f768a089fff19f0babbe67c73e24e5d3eb
Breno Hennrique <hennrique.breno@gmail.com>
2019-12-09 19:39:12 -0300

    Adiciona tratativa para o status da tarefa

--- src/intents/issue/getById.js
+++ src/intents/issue/getById.js
@@ -8,15 +8,39 @@ const getIssueById = (agent, _params) => {
     const { id } = agent.parameters
 
     RedmineHelper.getIssueById(id).then(response => {
+
+      let statusIssue = ''
+
+      switch (response.issue.status.name) {
+        case 'Nova':
+          statusIssue = `é nova`
+          break
+        case 'Em andamento':
+          statusIssue = `está em andamento`
+          break
+        case 'Resolvida':
+          statusIssue = `está resolvida`
+          break
+        case 'Feedback':
+          statusIssue = `é de feedback`
+          break
+        case 'Fechada':
+          statusIssue = `está fechada`
+          break
+        case 'Rejeitada':
+          statusIssue = `está rejeitada`
+      }
+
       const eventGetById = {
         name: 'EVENT_ISSUE_STATUS',
         parameters: {
-          status: response.issue.status.name
+          status: statusIssue
         },
         languageCode: 'pt-br'
       }
 
       agent.setFollowupEvent(eventGetById)
+      agent.setContext({ name: 'find_issue_id', lifespan: 5 })
       resolve()
     }, error => {
       debug('Erro ao buscar por tarefa baseado no id!', error)
